1999bc6584d263d4947b9beb007c5d646dee36e5
Motaz-Mukhtar <motazksa2@gmail.com>
2023-07-27 01:23:57 +0200

    Update /1-fifo_cache.py file.

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -14,9 +14,29 @@ class FIFOCache(BaseCaching):
 
     def put(self, key, item):
         """
+            Assign to the dict self.cache_data
+            the item value for the key 'key',
+            if key or item is None, this method
+            should ot do anything, if the number
+            of items in self.cache_data is higher
+            than BaseCaching.MAX_ITEMS, then you
+            must discard the first item in cache.
         """
-    if key is None or item is None:
-        return None
+        if key is None or item is None:
+            return None
+        self.cache_data[key] = item
 
-    if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-        
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            print("DISCARD: {}".format(list(self.cache_data)[0]))
+            del self.cache_data[list(self.cache_data)[0]]
+
+    def get(self, key, item):
+        """
+            Return the value in self.cache_data
+            linked to key, if key is None or if
+            the key doesn't exists in
+            self.cache_data, return None.
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
