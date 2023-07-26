56e15994d7cc333ec4aed6145c13a6238580645e
Motaz-Mukhtar <motazksa2@gmail.com>
2023-07-26 01:41:23 +0000

    Fix some pycodestyle issues, and remove the testing code.

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,47 +1,32 @@
 #!/usr/bin/env python3
 """
-	BasicCache Class
+    BasicCache Class
 """
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-	"""
-		Create BasicCache Class.
-	"""
-	def __inti__(self):
-		super().__init__()
+    """
+        Assign BasicCache Class.
+    """
+    def put(self, key, item):
+        """
+            Assign to the directory
+            self.cache_data the item
+            value for the key.
+        """
+        if key is None or item is None:
+            return None
+        self.cache_data[key] = item
 
-	def put(self, key, item):
-		"""
-		"""
-		if key is None or item is None:
-			return None
-		self.cache_data[key] = item
-
-	def get(self, key):
-		"""
-		"""
-		if (key is None or
-		    key not in self.cache_data):
-			return None
-		return self.cache_data[key]
-
-
-my_cache = BasicCache()
-my_cache.print_cache()
-my_cache.put("A", "Hello")
-my_cache.put("B", "World")
-my_cache.put("C", "Holberton")
-my_cache.print_cache()
-print(my_cache.get("A"))
-print(my_cache.get("B"))
-print(my_cache.get("C"))
-print(my_cache.get("D"))
-my_cache.print_cache()
-my_cache.put("D", "School")
-my_cache.put("E", "Battery")
-my_cache.put("A", "Street")
-my_cache.print_cache()
-print(my_cache.get("A"))
-
+    def get(self, key):
+        """
+            Return the value in
+            self.cache_data linked to
+            key, if kye is None or if the key
+            doesn't exists in self.cache_data
+            return None.
+        """
+        if (key is None or key not in self.cache_data):
+            return None
+        return self.cache_data[key]
